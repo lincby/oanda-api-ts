54a20477799c5b62ce70e0ddd5c7399c7741282a
lincby <lincby.com@gmail.com>
2020-08-23 18:28:20 +0200

    feature / adds account unit

---
+++ src/primitives/account.units.ts
@@ -0,0 +1,25 @@
+import {StringPrimitive} from './string.primitive';
+import {Decimal} from 'decimal.js';
+
+export class AccountUnits extends StringPrimitive {
+  constructor(accountUnits: AccountUnits);
+  constructor(accountUnits: Decimal);
+  constructor(accountUnits: string);
+  constructor(accountUnits: AccountUnits | Decimal | string) {
+    if (accountUnits instanceof AccountUnits) {
+      super(accountUnits.getValue());
+    } else if (accountUnits instanceof Decimal) {
+      super(accountUnits.toString());
+    } else {
+      super(accountUnits);
+    }
+  }
+
+  getDecimalValue(): Decimal {
+    return new Decimal(super.getValue());
+  }
+
+  copy(): AccountUnits {
+    return new AccountUnits(super.getValue());
+  }
+}
---
+++ test/primitives/account.units.spec.ts
@@ -0,0 +1,41 @@
+import {describe} from 'mocha';
+import {it} from 'mocha';
+import {expect} from 'chai';
+import {AccountUnits} from '../../src/primitives/account.units';
+import {Decimal} from 'decimal.js';
+import {DOUBLE_TEST_VALUE} from '../test.utils.spec';
+
+describe('AccountUnits', () => {
+  it('test string constructor and getter', () => {
+    const accountUnits: AccountUnits = createAccountUnits();
+    expectAccountUnits(accountUnits);
+  });
+
+  it('test account units constructor and getter', () => {
+    const accountUnits: AccountUnits = new AccountUnits(createAccountUnits());
+    expectAccountUnits(accountUnits);
+  });
+
+  it('test decimal constructor and getter', () => {
+    const accountUnits: AccountUnits = new AccountUnits(
+      new Decimal(DOUBLE_TEST_VALUE)
+    );
+    expectAccountUnits(accountUnits);
+  });
+
+  it('test copy', () => {
+    const accountUnits: AccountUnits = createAccountUnits();
+    const copyAccountUnits: AccountUnits = accountUnits.copy();
+    expect(copyAccountUnits).to.be.deep.equal(accountUnits);
+  });
+});
+
+export const createAccountUnits = () =>
+  new AccountUnits(new Decimal(DOUBLE_TEST_VALUE).toString());
+
+export const expectAccountUnits = (currency: AccountUnits) => {
+  expect(currency.getValue()).to.be.equal(DOUBLE_TEST_VALUE.toString());
+  expect(currency.getDecimalValue()).to.be.deep.equal(
+    new Decimal(DOUBLE_TEST_VALUE)
+  );
+};
